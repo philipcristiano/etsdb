bededba262fefe6d47b5748d285b3e6d67258b85
Philip Cristiano <philipcristiano@gmail.com>
2014-09-25 14:47:39 -0400

    graphite: Add example echo handler
    Need to add the protocol parsing the data from
    http://ninenines.eu/docs/en/ranch/1.0/guide/parsers/

--- src/etsdb_app.erl
+++ src/etsdb_app.erl
@@ -13,6 +13,8 @@ start(_StartType, _StartArgs) ->
     {ok, Pid} = etsdb_sup:start_link(),
     ok = riak_core:register([{vnode_module, etsdb_vnode}]),
     ok = riak_core_node_watcher:service_up(etsdb, self()),
+    {ok, _} = ranch:start_listener(graphite_input, 1,
+        ranch_tcp, [{port, 2003}], etsdb_graphite_protocol, []),
     {ok, Pid}.
 
 stop(_State) ->
