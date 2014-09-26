47e0168455765d340fc28dc5dd2cb7f0e433eb15
Philip Cristiano <philipcristiano@gmail.com>
2014-09-26 18:19:17 -0400

    Hello world http

--- src/etsdb_app.erl
+++ src/etsdb_app.erl
@@ -15,7 +15,19 @@ start(_StartType, _StartArgs) ->
     ok = riak_core_node_watcher:service_up(etsdb, self()),
     {ok, _} = ranch:start_listener(graphite_input, 1,
         ranch_tcp, [{port, 2003}], etsdb_graphite_protocol, []),
+    start_cowboy(),
     {ok, Pid}.
 
 stop(_State) ->
     ok.
+
+start_cowboy() ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", etsdb_handler_metrics, []},
+               {"/static/[...]", cowboy_static, {dir, "priv/static/"}},
+               {"/metrics", etsdb_handler_metrics, []}
+        ]}
+    ]),
+    cowboy:start_http(etsdb_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ).
